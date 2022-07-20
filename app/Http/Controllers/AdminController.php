3571144790e4683850f5f18008629d68f89f6cb0
Vladyslav <vlad.liubinetskyi@gmail.com>
2022-07-21 01:06:01 +0300

    init

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Appointment;
 use Illuminate\Http\Request;
-
 use App\Models\Doctor;
 
 class AdminController extends Controller
@@ -25,5 +25,66 @@ class AdminController extends Controller
 
         $doctor->save();
         return redirect()->back()->with('message', 'Complete');
+    }
+
+    public function showappointment(){
+        $data=Appointment::all();
+        return view('admin.showappointment', compact('data'));
+    }
+
+    public function approve($id){
+        $data=Appointment::find($id);
+        $data->status='Approved';
+        $data->save();
+
+        return redirect()->back();
+    }
+
+    public function cancel($id){
+        $data=Appointment::find($id);
+        $data->status='Canceled';
+        $data->save();
+
+        return redirect()->back();
+    }
+
+    public function showdoctor(){
+        $data=Doctor::all();
+        return view('admin.showdoctor', compact('data'));
+    }
+
+    public function updatedoctor($id){
+        $data=Doctor::find($id);
+        return view('admin.update_doctor', compact('data'));
+    }
+
+    public function deletedoctor($id){
+        $data=Doctor::find($id);
+        $data->delete();
+
+        return redirect()->back();
+    }
+    
+    public function editdoctor(Request $request, $id){
+        $doctor=Doctor::find($id);
+
+        $doctor->name=$request->name;
+        $doctor->phone=$request->phone;
+        $doctor->speciality=$request->specialty;
+        $doctor->office=$request->office;
+        $image=$request->file;
+        
+        if($image)
+        {
+
+        $imagename=time().'.'.$image->getClientOriginalExtension();
+        $request->file->move('doctorimage', $imagename);
+        $doctor->image=$imagename;
+        
+        }
+
+        $doctor->save();
+        
+        return redirect()->back()->with('message', 'Doctor Updated');
     }
 }
