95f6477efb14325bb11bd8a6a5faa0b4b9c2acd5
Vladyslav <vlad.liubinetskyi@gmail.com>
2022-07-21 20:03:51 +0300

    init

--- database/migrations/2022_07_19_083714_create_appointments_table.php
+++ database/migrations/2022_07_19_083714_create_appointments_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('date')->nullable();
             $table->string('message')->nullable();
             $table->string('status')->nullable();
-            $table->string('user_id')->nullable();
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
